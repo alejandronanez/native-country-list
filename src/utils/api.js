9243d29be3607270d6f820a67d63b412747eb6fd
Alejandro Nanez <alejonanez@gmail.com>
2018-02-27 20:28:01 -0500

    add prettier and eslint

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,14 +5,17 @@ import { type Country } from 'types/country';
 const API = 'https://restcountries.eu/rest/v2/all';
 
 export async function getCountries() {
-  const countries = await axios.get(API);
-
-  return countries;
+  return await axios.get(API);
 }
 
 function countriesHasher(countries: Array<Country>) {
-  return countries.reduce((countryHash, country) => ({
-    ...countryHash,
-    [country.region || 'No Region']: [...countryHash[country.region], country]
-  }), {});
+  return countries.reduce(
+    (countryHash, country) => ({
+      ...countryHash,
+      [country.region || 'No Region']: countryHash[country.region]
+        ? [...countryHash[country.region], country]
+        : []
+    }),
+    {}
+  );
 }
