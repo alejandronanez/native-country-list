d6588158dbe0a14550258b1039a5b5a33e3ce1d7
Alejandro Nanez <alejonanez@gmail.com>
2018-02-28 15:03:19 -0500

    get countries hash map from server response

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,25 @@
 import React, { Component } from 'react';
 import { Container } from 'native-base';
 import { Header } from 'nativeCountryList/src/components/Header/Header';
+import type { CountryHashMap } from 'nativeCountryList/src/types/country';
+import { getCountries } from 'nativeCountryList/src/utils/api';
 
 type Props = {};
-export default class App extends Component<Props> {
+type State = {
+  countries: CountryHashMap
+};
+export default class App extends Component<Props, State> {
+  state: State = {
+    countries: {}
+  };
+
+  async componentDidMount() {
+    const result = await getCountries();
+    this.setState(() => ({
+      countries: result
+    }));
+  }
+
   render() {
     return (
       <Container>
